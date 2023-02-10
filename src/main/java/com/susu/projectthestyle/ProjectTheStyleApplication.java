e0b692ec37b1aa3fb64743b7900dc00f193b5980
DevCheomJi <pjpjpjq@gmail.com>
2023-02-10 19:06:00 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/susu/projectthestyle/ProjectTheStyleApplication.java
@@ -0,0 +1,13 @@
+package com.susu.projectthestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTheStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTheStyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/susu/projectthestyle/ProjectTheStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susu.projectthestyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTheStyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
