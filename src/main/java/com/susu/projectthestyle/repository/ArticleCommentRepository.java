0e2953475e25422ecaa5a4251bc09aebbdbc3dd1
DevCheomJi <pjpjpjq@gmail.com>
2023-02-12 15:26:02 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가

--- src/main/java/com/susu/projectthestyle/repository/ArticleCommentRepository.java
+++ src/main/java/com/susu/projectthestyle/repository/ArticleCommentRepository.java
@@ -1,9 +1,30 @@
 package com.susu.projectthestyle.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.susu.projectthestyle.domain.ArticleComment;
+import com.susu.projectthestyle.domain.QArticle;
+import com.susu.projectthestyle.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>
+       ,QuerydslPredicateExecutor<ArticleComment>
+       , QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);//선택적인 필드에 대해서만 필터링 하기 위해 ..리스팅하지 않은 프로퍼티는 검색하지 않는 것을 true로
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); //
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+
+    }
 }
--- src/main/java/com/susu/projectthestyle/repository/ArticleRepository.java
+++ src/main/java/com/susu/projectthestyle/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.susu.projectthestyle.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.susu.projectthestyle.domain.Article;
+import com.susu.projectthestyle.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>
+       ,QuerydslPredicateExecutor<Article> //Article 에 있는 모든 필드에 대한 검색 기능을 넣어준다.
+       ,QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);//선택적인 필드에 대해서만 필터링 하기 위해 ..리스팅하지 않은 프로퍼티는 검색하지 않는 것을 true로
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); //
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+
+    }
 }
