d520a4f39332ecf10381bd09514f76230d91137a
DevCheomJi <pjpjpjq@gmail.com>
2023-02-12 23:56:48 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 '/login'컨트롤러를  테스트
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 @import 추가

---
+++ src/test/java/com/susu/projectthestyle/controller/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package com.susu.projectthestyle.controller;
+
+import com.susu.projectthestyle.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("View 컨트롤러  - 인증")
+@Import(SecurityConfig.class)
+@WebMvcTest // webMvcTest만으로도 Spring Security 테스트가 가능하다.
+
+public class AuthControllerTest {
+    private final MockMvc mvc;
+
+    AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+
+}
