07b75c2c4aaf0e8a59400d682d0532a9552003eb
DevCheomJi <pjpjpjq@gmail.com>
2023-02-12 23:24:47 +0900

    #18 회원 계정 도메인 구현 및 erd 반영

--- src/test/java/com/susu/projectthestyle/repository/JpaRepositoryTest.java
+++ src/test/java/com/susu/projectthestyle/repository/JpaRepositoryTest.java
@@ -5,14 +5,10 @@ import com.susu.projectthestyle.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 //@ActiveProfiles("testdb")
