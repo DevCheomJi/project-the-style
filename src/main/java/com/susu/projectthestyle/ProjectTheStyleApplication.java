778bebee0274d1ae7d505a50a6b75492cb990bc7
DevCheomJi <pjpjpjq@gmail.com>
2023-02-12 20:42:30 +0900

    #16 - thymeleaf 3 decoupled logic 설정
    자바 코드로 thymeleaf 3 decoupled logic을 
    사용자 어플리케이션 프로퍼티로 제어할 수 있게 추가

--- src/main/java/com/susu/projectthestyle/ProjectTheStyleApplication.java
+++ src/main/java/com/susu/projectthestyle/ProjectTheStyleApplication.java
@@ -2,7 +2,9 @@ package com.susu.projectthestyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ProjectTheStyleApplication {
 
