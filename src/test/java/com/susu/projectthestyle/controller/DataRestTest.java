409bc7ad4e7653b976a7e1443fcc8adb7f4be2dd
DevCheomJi <pjpjpjq@gmail.com>
2023-02-12 14:34:12 +0900

    #1 - dara rest 테스트
    꼭 필요한 테스트는 아니지만, 공부 겸 작성
    data rest 테스트는 인테그레이션으로 접근할 수 밖에 없다.
    DB 접근이 일어나므로 안전을 위해 @Transactional을 추가

---
+++ src/test/java/com/susu/projectthestyle/controller/DataRestTest.java
@@ -0,0 +1,73 @@
+package com.susu.projectthestyle.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//WebMvcTest < - slice test..Controller 외에 bean들을 load하지 않는다. 즉
+//DataRestAutoConfiguration을 읽지않았다. 그래서 에러 발생한다.
+//->그래서 integration code로 작성.(springboottest)
+//mock mvc 존재 알게 하기 위해 autoconfigureMockMvc추가
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional // 꼭 springframework 쪽을 import - TEST 에서 시작되는 트랜젝션은 기본적으로 [롤백]이다.
+@DisplayName("Data REST - API 테스트")
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
+        // Given
+
+        // When
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+
+    }
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticle_thenReturnsArticlesJsonResponse() throws Exception {
+        // Given
+
+        // When
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticlesJsonResponse() throws Exception {
+        // Given
+
+        // When
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+
+    }
+
+}
