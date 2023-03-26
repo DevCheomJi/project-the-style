db62a1b444c14be076523f00dcc2cb1c57929ff5
DevCheomJi <pjpjpjq@gmail.com>
2023-03-26 17:37:59 +0900

    gitflow-feature-stash: #23-entity-update
    회원 계정 엔티티와 게시글, 댓글 엔티티 연관관계 업데이트

--- src/main/java/com/susu/projectthestyle/domain/ArticleComment.java
+++ src/main/java/com/susu/projectthestyle/domain/ArticleComment.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString //쉽게 출력 관찰하기 위해
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -21,20 +21,21 @@ public class ArticleComment  extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql 에서는 identity로
     private Long id;
     @Setter @ManyToOne(optional = false) private Article article; // 댓글 삭제되더라도 게시글은 남아있어야 하니 cascade none(기본값) 설정
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 
     protected ArticleComment(){}
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content){
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
